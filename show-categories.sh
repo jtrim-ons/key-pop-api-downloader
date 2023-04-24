f23de0da56368f3fe1eb0a5b7957e08659290ef5
James Trimble <jtrim.ons@gmail.com>
2023-04-24 11:55:18 +0100

    Fix show-categories script

--- show-categories.sh
+++ show-categories.sh
@@ -6,7 +6,7 @@ cat classifications.txt | while read c; do
     echo $c
     filename=$(rg -l $c downloaded/classifications-*.json | head -n1)
     echo $filename
-    cat $filename | jq ".items[] | select(.id == \"resident_age_18b\") | .categories[] | .label"
-    echo $(cat $filename | jq ".items[] | select(.id == \"resident_age_18b\") | .categories[] | .label" | wc -l) categories.
+    cat $filename | jq ".items[] | select(.id == \"$c\") | .categories[] | .label"
+    echo $(cat $filename | jq ".items[] | select(.id == \"$c\") | .categories[] | .label" | wc -l) categories.
     echo
 done
