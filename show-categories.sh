d6db7c1e5d3d1ee5b0e9fe030321c9a3ebb3cdef
James Trimble <jtrim.ons@gmail.com>
2023-04-25 12:24:30 +0100

    Show both input and output categories

--- show-categories.sh
+++ show-categories.sh
@@ -2,11 +2,17 @@
 
 set -euo pipefail
 
-cat classifications.txt | while read c; do
-    echo $c
-    filename=$(rg -l $c downloaded/classifications-*.json | head -n1)
-    echo $filename
-    cat $filename | jq ".items[] | select(.id == \"$c\") | .categories[] | .label"
-    echo $(cat $filename | jq ".items[] | select(.id == \"$c\") | .categories[] | .label" | wc -l) categories.
+for f in input-classifications.txt output-classifications.txt; do
+    echo '========================================='
+    echo '  ' $f
+    echo '========================================='
     echo
+    cat $f | while read c; do
+        echo $c
+        filename=$(rg -l $c downloaded/classifications-*.json | head -n1)
+        echo $filename
+        cat $filename | jq ".items[] | select(.id == \"$c\") | .categories[] | .label"
+        echo $(cat $filename | jq ".items[] | select(.id == \"$c\") | .categories[] | .label" | wc -l) categories.
+        echo
+    done
 done
