8966e475b77754c6125b9c88e11f40ad8f8ed676
James Trimble <jtrim.ons@gmail.com>
2023-05-31 13:00:05 +0100

    Combine files on categories of last var

--- scripts/generate-files.py
+++ scripts/generate-files.py
@@ -1,6 +1,7 @@
 import gzip
 import itertools
 import json
+import os
 
 from key_pop_api_downloader import *
 
@@ -18,12 +19,12 @@ input_classifications.sort()
 
 def generate_outfile_path(cc, category_list):
     if len(cc) == 0:
-        filename_elements = ["data"]
-    else:
-        filename_elements = sum(
-            [[cat_id, opt['id']] for cat_id, opt in zip(cc, category_list)], []
-        )
-    return 'generated/{}var/{}.json'.format(len(cc), '-'.join(filename_elements))
+        raise "cc should have at least one element."
+
+    directory_names = [cat_id + '-' + opt['id'] for cat_id, opt in zip(cc, category_list)]
+    directory = 'generated/{}var/{}'.format(len(cc), '/'.join(directory_names))
+    os.makedirs(directory, exist_ok=True)
+    return directory + '/' + cc[-1] + '.json'
 
 
 def make_datum_key(cc, category_list, c, cat):
@@ -39,27 +40,44 @@ def make_datum_key(cc, category_list, c, cat):
     )
     return datum_key
 
+
+def generate_one_dataset(data, cc, category_list):
+    result = {}
+    for dataset in data:
+        c = dataset['c']
+        if dataset['data']['blocked']:
+            result[c] = "blocked"
+            continue
+        output_categories = all_classifications[c]['categories']
+        result[c] = {}
+        for cat in output_categories:
+            datum_key = make_datum_key(cc, category_list, c, cat)
+            result[c][cat['id']] = dataset['data'][datum_key]
+    return result
+
+
 def process_data(data, cc):
-    # category_lists is a list of tuples like (1, 4), which means that the first
-    # input variable has category 1 and the second input variable
-    # has category 4.
-    category_lists = itertools.product(
-        *(all_classifications[c_]["categories"] for c_ in cc)
-    )
-    for category_list in category_lists:
-        result = {}
-        for dataset in data:
-            c = dataset['c']
-            if dataset['data']['blocked']:
-                result[c] = "blocked"
-                continue
-            output_categories = all_classifications[c]['categories']
-            result[c] = {}
-            for cat in output_categories:
-                datum_key = make_datum_key(cc, category_list, c, cat)
-                result[c][cat['id']] = dataset['data'][datum_key]
-        with open(generate_outfile_path(cc, category_list), 'w') as f:
+    if len(cc) == 0:
+        result = generate_one_dataset(data, cc, [])
+        with open('generated/0var/data.json', 'w') as f:
             json.dump(result, f)
+    else:
+        # category_lists is a list of tuples like (1, 4), which means that the first
+        # input variable has category 1 and the second input variable
+        # has category 4.  We do this for all but the last element of the list cc.
+        #
+        # For the final variable in cc, we will generate a dataset for
+        # each value and combine these all in a single file.
+        category_lists = itertools.product(
+            *(all_classifications[c_]["categories"] for c_ in cc[:-1])
+        )
+        for category_list in category_lists:
+            result = {}
+            for last_var_category in all_classifications[cc[-1]]["categories"]:
+                dataset = generate_one_dataset(data, cc, (*category_list, last_var_category))
+                result[last_var_category['id']] = dataset
+            with open(generate_outfile_path(cc, category_list), 'w') as f:
+                json.dump(result, f)
 
 
 def data_to_lookup(data):
