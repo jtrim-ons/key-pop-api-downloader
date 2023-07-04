d981f3be7848825a27ca5e9e9a44a8d174376d80
James Trimble <jtrim.ons@gmail.com>
2023-07-04 11:33:36 +0100

    Download additional files with multiple input vars and no output vars

--- scripts/get-data.py
+++ scripts/get-data.py
@@ -6,11 +6,12 @@ import requests
 import sys
 import time
 
-from key_pop_api_downloader import *
+from key_pop_api_downloader import remove_classification_number
+from key_pop_api_downloader import get_input_classification_combinations
 
 url_pattern = "https://api.beta.ons.gov.uk/v1/population-types/UR/census-observations?area-type=nat&dimensions={}&limit=10000000"
 
-skip_existing_files = sys.argv[1] == '--skip-existing'
+skip_existing_files = '--skip-existing' in sys.argv
 
 with open('input-txt-files/output-classifications.txt', 'r') as f:
     output_classifications = f.read().splitlines()
@@ -20,22 +21,36 @@ with open('input-txt-files/input-classifications.txt', 'r') as f:
     input_classifications = f.read().splitlines()
 input_classifications.sort()
 
-for num_vars in range(0, 4):
-    input_classification_combinations = get_input_classification_combinations(input_classifications, num_vars)
-    for i, cc in enumerate(input_classification_combinations):
-        for j, c in enumerate(output_classifications if num_vars > 0 else list(set(input_classifications + output_classifications))):
-            if remove_classification_number(c) in [remove_classification_number(c_) for c_ in cc]:
-                # The API won't give data for two versions of the same variable
-                continue
-            c_str = ",".join(list(cc) + [c])
-            compressed_file_path = 'downloaded/{}var/{}.json.gz'.format(num_vars, c_str.replace(',', '-'))
-            if skip_existing_files and os.path.isfile(compressed_file_path):
-                print("{} var: Skipping existing file {} of {} ({})".format(num_vars, i+1, len(input_classification_combinations), c_str))
-                continue
-            print("{} var: Downloading {} of {} ({})".format(num_vars, i+1, len(input_classification_combinations), c_str))
-            url = url_pattern.format(c_str)
-            response = requests.get(url, stream=True)
-            response_bytes = response.content
-            with gzip.open(compressed_file_path, 'wb') as f:
-                f.write(response_bytes)
-            time.sleep(0.5)
+
+def get_file(compressed_file_path, c_str):
+    if skip_existing_files and os.path.isfile(compressed_file_path):
+        print("Skipping existing file {}".format(compressed_file_path))
+        return
+    print("Downloading {}".format(compressed_file_path))
+    url = url_pattern.format(c_str)
+    response = requests.get(url, stream=True)
+    response_bytes = response.content
+    with gzip.open(compressed_file_path, 'wb') as f:
+        f.write(response_bytes)
+    time.sleep(0.5)
+
+
+def main():
+    for num_vars in range(0, 4):
+        input_classification_combinations = get_input_classification_combinations(input_classifications, num_vars)
+        for i, cc in enumerate(input_classification_combinations):
+            if num_vars > 0:
+                c_str = ",".join(cc)
+                compressed_file_path = 'downloaded/{}var/{}.json.gz'.format(num_vars, c_str.replace(',', '-'))
+                get_file(compressed_file_path, c_str)
+            for j, c in enumerate(output_classifications):
+                if remove_classification_number(c) in [remove_classification_number(c_) for c_ in cc]:
+                    # The API won't give data for two versions of the same variable
+                    continue
+                c_str = ",".join(list(cc) + [c])
+                compressed_file_path = 'downloaded/{}var/{}.json.gz'.format(num_vars, c_str.replace(',', '-'))
+                get_file(compressed_file_path, c_str)
+
+
+if __name__ == "__main__":
+    main()
